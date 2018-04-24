e5f9e311ea103e5693d98309598341b54661a211
Mellina Y <pong7219@gmail.com>
2018-04-24 18:35:05 -0300

    Novas Páginas

--- js/script.js
+++ js/script.js
@@ -28,6 +28,61 @@ $(document).ready(function(){
      scroll_start = $(this).scrollTop();
      if(scroll_start > offset.top) {
          $('.topbar').addClass('scrolled');
+         $('#btnTopo').addClass('show');
+      } else {
+        $('.topbar').removeClass('scrolled');
+        $('#btnTopo').removeClass('show');
+      }
+  });
+});
+
+// Smooth scroll
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 80
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+// Top button
+$(document).ready(function(){       
+  var scroll_start = 0;
+  var startchange = $('header');
+  var offset = startchange.offset();
+  $(document).scroll(function() { 
+     scroll_start = $(this).scrollTop();
+     if(scroll_start > offset.top) {
+         $('.topbar').addClass('scrolled');
       } else {
         $('.topbar').removeClass('scrolled');
       }
