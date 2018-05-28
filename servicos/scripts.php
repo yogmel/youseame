4d033129c4ca8f9b51ba52c1c5b029b7e1c0b7cf
yogmel <yonashiro.mellina@gmail.com>
2018-05-28 15:43:26 -0300

    correção de https

--- servicos/scripts.php
+++ servicos/scripts.php
@@ -1,4 +1,4 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="../assets/js/vertical-timeline.js"></script>
 <script src="../assets/fonts/fontawesome-all.min.js"></script>
 <script src="../assets/js/script.js"></script>
